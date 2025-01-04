2159e18b7bf69e142f274742613e10f83a9ee13d
Rachida El Hady <elhadyrachida71@gmail.com>
2025-01-04 16:00:24 +0300

    `Removed unused code and commented out sections in Angular project`

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,27 +5,6 @@ import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, },
-
-  {
-    path: 'bootstrap',
-    loadChildren: () => import(`./features/tutorials/example-bootstrap/tutorial.routes`)
-    .then(routes => routes.routes)
-  },
-  {
-    path: 'components',
-    loadComponent: () => import('./features/tutorials/example-components/tutorial.component')
-      .then(mod => mod.TutorialComponent)
-  },
-  {
-    path: 'forms',
-    loadChildren: () => import(`./features/tutorials/example-forms/tutorial.routes`)
-    .then(routes => routes.routes)
-  },
-  {
-    path: 'services',
-    loadComponent: () => import('./features/tutorials/example-services/tutorial.component')
-      .then(mod => mod.TutorialComponent)
-  },
-
+  
   { path: '**', component: NotFoundComponent }
 ];
